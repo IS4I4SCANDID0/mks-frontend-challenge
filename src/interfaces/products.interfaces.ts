17ff70a3ace723fc5b2e1ace3fb1a93bde550b7d
Isaias Candido <isaiascandido92@outlook.com>
2024-04-26 22:17:35 -0300

    feat: adiciona a estrutura básica de pastas e arquivos, também adiciona as  bibliotecas que serão utilizadas no projeto

---
+++ src/interfaces/products.interfaces.ts
@@ -0,0 +1,9 @@
+interface IProducts {
+  products :{
+    id: number;
+    img: string;
+    name: string;
+    description: string;
+    price: number
+  }
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+// import { queryClient } from "@/services/queryClinte";
+import { QueryClient, QueryClientProvider, QueryClientProviderProps } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import React from "react";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = React.useState(() => {
+    
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          staleTime: 60 * 1000
+        },
+      }
+    })
+  })
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
